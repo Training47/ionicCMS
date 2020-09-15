82ea4943a80177355693dce10f6a8e12ed280df4
Lisa Pena <lisapena13@gmail.com>
2020-09-15 10:54:05 -0500

    Clean Up and Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,22 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Login',
+      url: '/login',
+      icon: 'log-in'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Logout',
+      url: '/logout',
+      icon: 'log-out'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
+      title: 'Register',
+      url: '/register',
+      icon: 'person-add'
     },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Login', 'Logout', 'Register', 'User', 'Users'];
 
   constructor(
     private platform: Platform,
